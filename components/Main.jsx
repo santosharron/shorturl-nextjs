12d259e619969be328f20c65845ba23bb8fddfe4
Santosh Arron <73644573+santosharron@users.noreply.github.com>
2024-09-07 17:43:57 +0530

    fxied bg

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,7 +5,7 @@ import Footer from './Footer';
 export default function Main() {
     return (
         <>
-        <div className="bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
+        <div className="min-h-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]">
             <Page />
             <Form />
             <Footer />
